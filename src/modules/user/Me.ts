a685fd0401846a8b7252b9e2af66b6edda7f950d
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-05 20:31:13 +0530

    me resolver

---
+++ src/modules/user/Me.ts
@@ -0,0 +1,14 @@
+import { MyContext } from "src/types/MyContext";
+import { Ctx, Query, Resolver } from "type-graphql";
+import { User } from "../../entity/User";
+
+@Resolver()
+export class MeResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() ctx: MyContext): Promise<User | undefined> {
+    const id = ctx.req.session.userId;
+    if (!id) return undefined;
+
+    return User.findOne(id);
+  }
+}

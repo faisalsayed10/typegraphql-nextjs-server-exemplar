d8ae3fd418410ea2f7cb5f633f2819c13dc6f0e0
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-05 20:31:27 +0530

    email and confirmation

---
+++ src/modules/user/ConfirmUser.ts
@@ -0,0 +1,22 @@
+import { User } from "../../entity/User";
+import { MyContext } from "src/types/MyContext";
+import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
+import { redis } from "../../redis";
+
+@Resolver()
+export class ConfirmUserResolver {
+  @Mutation(() => Boolean)
+  async confirmUser(
+    @Arg("token") token: string,
+    @Ctx() ctx: MyContext
+  ): Promise<boolean> {
+    const userId = await redis.get(token);
+
+    if (!userId) return false;
+
+    await User.update({ id: parseInt(userId, 10) }, { confirmed: true });
+    await redis.del(token);
+
+    return true;
+  }
+}

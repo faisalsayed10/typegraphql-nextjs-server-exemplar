b9158dbb3ddc3e02ef13631737b21ba7a75e5e0a
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-06 18:55:20 +0530

    ChangePassword resolver

---
+++ src/modules/user/ChangePassword.ts
@@ -0,0 +1,31 @@
+import { hash } from "bcryptjs";
+import { MyContext } from "src/types/MyContext";
+import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
+import { User } from "../../entity/User";
+import { redis } from "../../redis";
+import { forgotPasswordPrefix } from "../constants/redisPrefixes";
+import { ChangePasswordInput } from "./changePassword/ChangePasswordInput";
+
+@Resolver()
+export class ChangePasswordResolver {
+  @Mutation(() => User, { nullable: true })
+  async changePassword(
+    @Arg("data") { token, password }: ChangePasswordInput,
+    @Ctx() ctx: MyContext
+  ): Promise<User | null> {
+    const userId = await redis.get(forgotPasswordPrefix + token);
+    if (!userId) return null;
+
+    const user = await User.findOne(userId);
+    if (!user) return null;
+
+    await redis.del(forgotPasswordPrefix + token);
+
+    user.password = await hash(password, 12);
+    await user.save();
+
+    ctx.req.session.userId = user.id;
+
+    return user;
+  }
+}

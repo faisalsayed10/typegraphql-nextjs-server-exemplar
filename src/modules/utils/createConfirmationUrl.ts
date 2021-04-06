827a1b29cf845d0c9a28c1c0c03e970f7c415dd6
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-06 18:55:02 +0530

    minor changes and common input

--- src/modules/utils/createConfirmationUrl.ts
+++ src/modules/utils/createConfirmationUrl.ts
@@ -1,9 +1,10 @@
 import { redis } from "../../redis";
 import { v4 } from "uuid";
+import { confirmationPrefix } from "../constants/redisPrefixes";
 
 export const createConfirmationUrl = async (id: number) => {
   const token = v4();
-  await redis.set(token, id, "ex", 60 * 60 * 24);
+  await redis.set(confirmationPrefix + token, id, "ex", 60 * 60 * 24);
 
   return `http://localhost:3000/user/confirm/${token}`;
 };
